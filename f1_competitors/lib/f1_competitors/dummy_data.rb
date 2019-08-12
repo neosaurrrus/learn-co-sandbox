ffa9a06f6dceb0cb3c1a70f24a45768978184c18
Lukie Kang <lukiek+f@gmail.com>
2019-08-12 06:09:42 +0000

    Done.

--- f1_competitors/lib/f1_competitors/dummy_data.rb
+++ f1_competitors/lib/f1_competitors/dummy_data.rb
@@ -1,6 +1,40 @@
 class Dummy # This returns dummy data for testing the CLI without actually scraping anything
 
+  def drivers_list
+    self.drivers = [
+    "Andrew Aaronson",
+    "Billy Belcher", 
+    "Carly Cat", 
+    "Denise Dam",
+    "Egbert Echo",
+    "Fred Foxtrot", 
+    "Gemma Gamma", 
+    "Harriet Hotel", 
+    "Ingrid India", 
+    "Jason June", 
+    "Kevin Kilo",
+    "Lisa Lamp",
+    "Mike Magpie",
+    "Ned November",
+    "Oscar Oscar",
+    "Petra Pint",
+    "Quentin Quick",
+    "Rusty Robot",
+    "Susan Smith",
+    "Terry Train"
+    ]
 
-
+def driver_details = 
+  team: "Alfa Romeo"
+  country: "Finland",
+  codiums: "103",
+  points:	"1847",
+  grands_prix_entered: "306",
+  world_championships:"1",
+  highest_race_finish:"1 (x21)",
+  highest_grid_position: "1",
+  date_of_birth: "17/10/1979",
+  place_of_birth:	"Espoo, Finland"
+}
 
 end # of class
